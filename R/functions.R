2a893042db545eb24627f564135a0365f021c3cc
Jorge Galindo <jorgegalindoalfonso@gmail.com>
2023-01-04 17:40:34 +0100

    Theme_ecpol_v2
    Big update:
    - Tamaño de letra por defecto más peque para caption y ejes
    - Fuera títulos de ejes por defecto
    - Algo más de distancia entre el caption y el eje x
    
    Además, paletas nuevas :D

--- R/functions.R
+++ R/functions.R
@@ -15,29 +15,27 @@ theme_ecpol <- function(){
     plot.subtitle = element_text(hjust = 0, size = 14,
                                   family="Mabry Pro Light",
                                   color = "#000000"),
-    plot.caption = element_text(hjust = 0, size = 11,
+    plot.caption = element_text(hjust = 0.2, size = 10,
                                 vjust = .5,
                                 family="Mabry Pro Light",
                                 color = "#777777"),
     legend.position="top",
     legend.key = element_rect(fill = "transparent", colour = "transparent"),
     legend.title = element_blank(),
-    axis.title.x = element_text(hjust = 0.5, size = 13,
-                                family="Mabry Pro Light",
-                                color = "#444444"),
-    axis.title.y = element_text(angle = 0, size= 13,
-                                family="Mabry Pro Light",
-                                color = "#444444"),
-    axis.text.x = element_text(size = 13,
+    axis.title.x = element_blank(),
+    axis.title.y = element_blank(),
+    axis.text.x = element_text(size = 12,
                                family="Mabry Pro Light"),
-    axis.text.y = element_text(size = 13,
+    axis.text.y = element_text(size = 12,
                                color = "grey27",
                                family="Mabry Pro Light"),
+    strip.text = element_text(face = "Mabry Pro Regular", size = 12),
+    strip.background = element_rect(fill = "transparent")
   )
 }
 
 
-##Palette
+#Basic discrete palettes
 
 palettes_ecpol <- list(
   ksnet_ecpol = c("#00B2A9"),
@@ -50,50 +48,26 @@ palettes_ecpol <- list(
   semaforo_inverted = c("#00A47D","#EFB43F","#E05656"),
   semaforo = c("#00A47D","#EFB43F","#E05656"),
   fourcolors = c("#E05656","#EFB43F","#00A47D","#225E9C"),
-  fivecolors = c("#E05656","#EFB43F","#00A47D","#225E9C","#404040"),
-  sixcolors = c("#E6007E","#E05656","#EFB43F","#00A47D","#225E9C","#404040"),
-  sevencolors = c("#E6007E","#E05656","#F27200","#EFB43F","#00A47D","#225E9C","#404040"),
-  eightcolors = c("#E6007E","#E05656","#F27200","#EFB43F","#00A47D","#225E9C","#404040","#1a1a1a"),
-  ninecolors = c("#E6007E","#E05656","#F27200","#EFB43F","#00A47D","#225E9C","#4B2991","#404040","#1a1a1a"),
-  tencolors = c("#952EA0","#E6007E","#E05656","#F27200","#EFB43F","#00A47D","#225E9C","#4B2991","#404040","#1a1a1a"),
-  elevencolors = c("#952EA0","#E6007E","#E05656","#F27200","#EFB43F","#00A47D","#225E9C","#4B2991","#1d475c","#404040","#1a1a1a"),
-  cont_short = c("#efb43f", "#beb963", "#9ab776", "#7cb282", "#62ac8a", "#4aa58e", "#359d91", "#1f9592", "#0b8c92", "#0c8390", "#0d7a8c", "#0e7188", "#0e6983", "#0d607e", "#0d5878", "#0c5073", "#0b486d", "#0a4066", "#083860", "#06305a", "#052954", "#03224d", "#011b47", "#001440", "#000a3a"),
-  cont_long = c("#efb43f", "#ddb24c", "#cdb057", "#bcad61", "#adaa69", "#9da66f", "#8fa275", "#809e7a", "#73997e", "#659582", "#589084", "#4b8a86", "#3e8588", "#328088", "#257a89", "#177488", "#0e6f86", "#0f6982", "#0f637e", "#0f5d7a", "#0f5776", "#0e5172", "#0e4c6d", "#0d4669", "#0c4065", "#0b3b61", "#0a365c", "#083058", "#072b54", "#05264f", "#04214b", "#021c47", "#001742", "#00113e", "#000a3a"
-)
   )
-palette_ecpol <- function(name, n, type = c("discrete", "continuous")) {
-  type <- match.arg(type)
 
+
+palette_ecpol <- function(name, n) {
   pal <- palettes_ecpol[[name]]
   if (is.null(pal))
-    stop("De que me hablas nano")
+    stop("De qué me hablas nano")
 
   if (missing(n)) {
     n <- length(pal)
   }
 
-  if (type == "discrete" && n > length(pal)) {
+  if (n > length(pal)) {
     stop("No hay tanto color")
   }
 
-  out <- switch(type,
-                continuous = grDevices::colorRampPalette(pal)(n),
-                discrete = pal[1:n]
-  )
+  out <- pal[1:n]
   structure(out, class = "palette", name = name)
 }
 
-print.palette <- function(x, ...) {
-  n <- length(x)
-  old <- par(mar = c(0.5, 0.5, 0.5, 0.5))
-  on.exit(par(old))
-
-  image(1:n, 1, as.matrix(1:n), col = x,
-        ylab = "", xaxt = "n", yaxt = "n", bty = "n")
-
-  rect(0, 0.9, n + 1, 1.1, col = rgb(1, 1, 1, 0.8), border = NA)
-  text((n + 1) / 2, 1, labels = attr(x, "name"), cex = 1, family = "serif")
-}
 
 palette_ksnet <- palette_ecpol("ksnet_ecpol")
 palette_ksnet_two <- palette_ecpol("ksnet_ecpol_two")
@@ -105,14 +79,38 @@ palette_redgreen <- palette_ecpol("redgreen")
 palette_greenred <- palette_ecpol("greenred")
 palette_three <- palette_ecpol("threecolors")
 palette_four <- palette_ecpol("fourcolors")
-palette_five <- palette_ecpol("fivecolors")
-palette_six <- palette_ecpol("sixcolors")
-palette_seven <- palette_ecpol("sevencolors")
-palette_eight <- palette_ecpol("eightcolors")
-palette_nine <- palette_ecpol("ninecolors")
-palette_ten <- palette_ecpol("tencolors")
-palette_eleven <- palette_ecpol("elevencolors")
-palette_cont_short <- palette_ecpol("cont_short")
-palette_cont_long <- palette_ecpol("cont_long")
 
+#Gradient palettes
+
+gradient_palettes <- list(
+gradient_palette_red = c("#E05656", "#F47D7D" , "#FCA55F", "#FFCE7A" , "#efb43f", "#ddb24c", "#cdb057", "#bcad61", "#adaa69", "#9da66f", "#8fa275", "#809e7a", "#73997e", "#659582", "#589084", "#4b8a86", "#3e8588", "#328088", "#257a89", "#177488", "#0e6f86", "#0f6982", "#0f637e", "#0f5d7a"),
+gradient_palette = c("#efb43f", "#ddb24c", "#cdb057", "#bcad61", "#adaa69", "#9da66f", "#8fa275", "#809e7a", "#73997e", "#659582", "#589084", "#4b8a86", "#3e8588", "#328088", "#257a89", "#177488", "#0e6f86", "#0f6982", "#0f637e", "#0f5d7a", "#0f5776", "#0e5172", "#0e4c6d", "#0d4669", "#0c4065", "#0b3b61", "#0a365c", "#083058", "#072b54", "#05264f", "#04214b", "#021c47", "#001742", "#00113e", "#000a3a")
+)
+
+gradient_palette_red <- colorRampPalette(gradient_palettes$gradient_palette_red)
+gradient_palette <- colorRampPalette(gradient_palettes$gradient_palette)
+
+gradient_loop <- 3:20
+
+for (i in gradient_loop) {
+  assign(paste0("gradient_palette_", i), colorRampPalette(gradient_palettes$gradient_palette)(i))
+  assign(paste0("gradient_palette_red_", i), colorRampPalette(gradient_palettes$gradient_palette_red)(i))
+}
+
+
+#Printing palettes
+
+print_palette <- function(x, ...) {
+  n <- length(x)
+  old <- par(mar = c(0.5, 0.5, 0.5, 0.5))
+  on.exit(par(old))
+
+  image(1:n, 1, as.matrix(1:n), col = x,
+        ylab = "", xaxt = "n", yaxt = "n", bty = "n")
+
+  rect(0, 0.9, n + 1, 1.1, col = rgb(1, 1, 1, 0.8), border = NA)
+  text((n + 1) / 2, 1, labels = attr(x, "name"), cex = 1, family = "serif")
+}
+
+print_gradient<-outer(1:20,1:20,function(x,y) sin(sqrt(x*y)/3))
 
