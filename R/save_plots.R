0a1b0cf9c58620baea4c0c1acb20e1cbdae8837e
Javi Martinez <javier.martinez.snt@gmail.com>
2023-06-15 18:46:20 +0200

    Create save functions

---
+++ R/save_plots.R
@@ -0,0 +1,37 @@
+# Función para guardar gráficos en PNG
+
+png_ecpol <- function(plot, filename = "", width = 18, height = 12, unit = "cm", dpi = 300) {
+  ggsave(
+    plot = plot,
+    filename = filename,
+    device = 'png',
+    width = width,
+    height = height,
+    unit = unit,
+    dpi = dpi
+  )
+}
+
+jpg_ecpol <- function(plot, filename = "", width = 18, height = 12, unit = "cm", dpi = 300) {
+  ggsave(
+    plot = plot,
+    filename = filename,
+    device = 'jpg',
+    width = width,
+    height = height,
+    unit = unit,
+    dpi = dpi
+  )
+}
+
+pdf_ecpol <- function(plot, filename = "", width = 18, height = 12, unit = "cm", dpi = 300) {
+  ggsave(
+    plot = plot,
+    filename = filename,
+    device = cairo_pdf,
+    width = width,
+    height = height,
+    unit = unit,
+    dpi = dpi
+  )
+}
