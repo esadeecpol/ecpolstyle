d1a287fb62892dc1a40a8d9f28481fe8e973774a
Javi Martinez <javier.martinez.snt@gmail.com>
2023-09-09 08:56:32 +0200

    Create directory function

---
+++ R/directory_ecpol.R
@@ -0,0 +1,24 @@
+
+# Funcion para crear toda la estructura de las carpetas de un paper
+
+directory_ecpol <- function() {
+  # Directorio base
+  base_dir <- getwd() # Obtenemos el directorio actual
+  
+  # Lista de carpetas
+  folders <- c("code", "input/raw", "input/data", "output/tables", "output/figures/png", "output/figures/svg", "content")
+  
+  # Crear carpetas
+  for (folder in folders) {
+    dir.create(file.path(base_dir, folder), recursive = TRUE, showWarnings = FALSE)
+  }
+  
+  # Crear archivo .gitignore
+  gitignore_content <- "*.csv\n*.dta\n*.rds"
+  cat(gitignore_content, file = file.path(base_dir, ".gitignore"), sep = "\n")
+  
+  cat("Directorio creado exitosamente.\n")
+}
+
+# Llama a la función para crear las carpetas y el archivo .gitignore
+directory_ecpol()
